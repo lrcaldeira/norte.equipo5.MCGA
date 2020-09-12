c8e6eaca1fb554afdbfb846c60b875b96f6b8b90
Nicolas <nicolas.lopez97@hotmail.com>
2020-09-12 19:05:57 -0300

    Se reestructura proyecto

---
+++ ArtShopProyect/Hosts/ArtShop.Hosts.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+
+namespace ArtShop.Hosts.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Remove "Controller" suffix.
+            var suffix = typeof(DefaultHttpControllerSelector).GetField("ControllerSuffix", BindingFlags.Static | BindingFlags.Public);
+            if (suffix != null) suffix.SetValue(null, string.Empty);
+
+            config.Services.Replace(typeof(IHttpControllerTypeResolver),
+               new HttpServiceTypeResolver());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
