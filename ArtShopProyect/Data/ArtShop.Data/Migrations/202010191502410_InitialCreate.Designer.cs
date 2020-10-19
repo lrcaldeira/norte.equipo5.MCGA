afc3b75aea48db459d345be795280b5794d9897a
Luciana Rodriguez Caldeira <lurc85@gmail.com>
2020-10-19 15:59:06 -0300

    Cambio de Estilo

--- ArtShopProyect/Data/ArtShop.Data/Migrations/202010191502410_InitialCreate.Designer.cs
+++ ArtShopProyect/Data/ArtShop.Data/Migrations/202010191502410_InitialCreate.Designer.cs
@@ -7,13 +7,13 @@ namespace ArtShop.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class tp : IMigrationMetadata
+    public sealed partial class InitialCreate : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(tp));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
         
         string IMigrationMetadata.Id
         {
-            get { return "202010112323111_tp"; }
+            get { return "202010191502410_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source
--- ArtShopProyect/Data/ArtShop.Data/Migrations/202010191502410_InitialCreate.cs
+++ ArtShopProyect/Data/ArtShop.Data/Migrations/202010191502410_InitialCreate.cs
@@ -3,7 +3,7 @@
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class tp : DbMigration
+    public partial class InitialCreate : DbMigration
     {
         public override void Up()
         {
