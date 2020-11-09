d054d8b4f8660edf78eef8a28c12ac1b1aa525bb
Ferponce <fernyeponce@gmail.com>
2020-11-08 23:12:09 -0300

    Corrección Menu segun usuario

--- ArtShopProyect/Presentation/ArtShop.UI.Web/Controllers/ShopController.cs
+++ ArtShopProyect/Presentation/ArtShop.UI.Web/Controllers/ShopController.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ArtShop.UI.Process;
 
 namespace ArtShop.UI.Web.Controllers
 {
     public class ShopController : BaseController
     {
         // GET: Shop
-
 
 
         public ActionResult Index()
