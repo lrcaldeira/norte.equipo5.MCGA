82956d34d90ad8d4e7558cdf5a0b2c9e5434376c
Ferponce <fernyeponce@gmail.com>
2020-11-14 00:20:41 -0300

    Agrego CartBusiness, CartItemBusiness, BusinessException, y Exceptions en Framework

---
+++ ArtShopProyect/Business/ArtShop.Business/CartBusiness.cs
@@ -0,0 +1,46 @@
+﻿using ArtShop.Data;
+using ArtShop.Entities.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtShop.Business
+{
+    public class CartBusiness
+    {
+        private BaseDataService<Cart> db = new BaseDataService<Cart>();
+        CartDAC cartdac = new CartDAC();
+
+        public Cart CrearCarrito(Cart cart)
+        {
+
+            Cart update = cart.Id > 0 ? cartdac.SelectById(cart.Id) : new Cart { CreatedOn = DateTime.Now };
+            
+            update.CartDate = cart.CartDate == null ? throw new BusinessException("b.validation.cart.cartdate.invalid") : cart.CartDate;
+            update.Cookie = string.IsNullOrEmpty(cart.Cookie)? throw new BusinessException("b.validation.cart.cookie.invalid"): cart.Cookie;
+            update.ChangedOn = DateTime.Now;
+            update.ChangedBy = cart.ChangedBy;
+            var saved = cartdac.Create(cart);
+            return saved;
+        }
+
+        public void Borrar(int id)
+        {
+            cartdac.DeleteById(id);
+        }
+
+        public List<Cart> Listar()
+        {
+            var result = cartdac.Select();
+            return result;
+        }
+
+        public Cart Buscar(int id)
+        {
+            return cartdac.SelectById(id);
+        }
+    }
+}
---
+++ ArtShopProyect/Business/ArtShop.Business/CartItemBusiness.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using ArtShop.Entities;
+using ArtShop.Data;
+using ArtShop.Entities.Model;
+
+namespace ArtShop.Business
+{
+    public class CartItemBusiness
+    {
+        private BaseDataService<CartItem> db = new BaseDataService<CartItem>();
+
+        CartItemDAC cartitemdac = new CartItemDAC();
+        ProductBusiness productbusiness = new ProductBusiness();
+        CartBusiness cartbusiness = new CartBusiness();
+
+        public CartItem Crearitem(CartItem cartitem)
+        {
+            CartItem update = cartitem.Id > 0 ? cartitemdac.SelectById(cartitem.Id) : new CartItem { CreatedOn = DateTime.Now };
+
+            if (cartitem.ProductId <= 0)
+            {
+                throw new BusinessException("b.validation.cartitem.productId.missing");
+            }
+
+
+            var product = productbusiness.GetProduct(cartitem.ProductId);
+            if (product == null)
+            {
+                throw new BusinessException("b.validation.cartitem.productId.invalid");
+            }
+
+            update.ProductId = cartitem.ProductId;
+
+            if (cartitem.CartId <= 0) throw new BusinessException("b.validation.cartitem.cartId.invalid");
+            if (cartbusiness.Buscar(cartitem.CartId) == null) throw new BusinessException("b.validation.cartitem.cartId.invalid");
+
+            update.CartId = cartitem.CartId;
+
+
+            update.Price = cartitem.Price <= 0
+                ? throw new BusinessException("b.validation.cartitem.price.invalid")
+                : cartitem.Price;
+
+            update.Quantity = cartitem.Quantity <= 0
+                ? throw new BusinessException("b.validation.cartitem.quantity.invalid")
+                : cartitem.Quantity;
+
+
+            update.ChangedOn = DateTime.Now;
+            update.ChangedBy = cartitem.ChangedBy;
+
+            var saved = cartitemdac.Create(cartitem);
+            return saved;
+        }
+
+        public void Borrar(int id)
+        {
+            cartitemdac.DeleteById(id);
+        }
+
+        public List<CartItem> Listar()
+        {
+            var result = cartitemdac.Select();
+            return result;
+        }
+        public CartItem Buscar(int id)
+        {
+            return cartitemdac.SelectById(id);
+        }
+    }
+}
